a4e047c2ae5fead4d27e2769a1b71ca4ca25d794
suthakaranburaj <suthakaranburaj@gmail.com>
2025-10-25 14:01:49 +0530

    gemini model changed

--- client/src/service/AIModel.jsx
+++ client/src/service/AIModel.jsx
@@ -8,7 +8,7 @@ import {
     const genAI = new GoogleGenerativeAI(apiKey);
     
     const model = genAI.getGenerativeModel({
-        model: "gemini-1.5-flash",
+        model: "gemini-2.5-flash",
     });
     
     const generationConfig = {
